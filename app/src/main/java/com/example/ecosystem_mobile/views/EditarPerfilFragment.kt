69bde8d782daf0b0292387072e7fd49fa72c7cc8
Carlos Augusto Da Silva Santos <code.carlosaugusto@gmail.com>
2024-02-02 21:46:14 -0300

    Iniciando as configurações de layout

--- app/src/main/java/com/example/ecosystem_mobile/views/EditarPerfilFragment.kt
+++ app/src/main/java/com/example/ecosystem_mobile/views/EditarPerfilFragment.kt
@@ -1,17 +1,15 @@
-package com.example.ecosystem_mobile.ui.home
+package com.example.ecosystem_mobile.views
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.ecosystem_mobile.databinding.FragmentHomeBinding
+import com.example.ecosystem_mobile.databinding.FragmentEditarPerfilBinding
 
-class HomeFragment : Fragment() {
+class EditarPerfilFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentEditarPerfilBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,7 +21,7 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentEditarPerfilBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         return root
--- app/src/main/java/com/example/ecosystem_mobile/views/InstitucionalFragment.kt
+++ app/src/main/java/com/example/ecosystem_mobile/views/InstitucionalFragment.kt
@@ -1,17 +1,15 @@
-package com.example.ecosystem_mobile.ui.gallery
+package com.example.ecosystem_mobile.views
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.ecosystem_mobile.databinding.FragmentGalleryBinding
+import com.example.ecosystem_mobile.databinding.FragmentInstitucionalBinding
 
-class GalleryFragment : Fragment() {
+class InstitucionalFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentInstitucionalBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,7 +21,7 @@ class GalleryFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentInstitucionalBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         return root
