047e4cd69af2015e22cc2bc65cf079fc0526cd5b
Carlos Augusto Da Silva Santos <code.carlosaugusto@gmail.com>
2024-02-05 21:51:21 -0300

    Continuação das configurações de layout

--- app/src/main/java/com/example/ecosystem_mobile/MainActivity.kt
+++ app/src/main/java/com/example/ecosystem_mobile/MainActivity.kt
@@ -26,25 +26,20 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_editar_perfil, R.id.nav_institucional, R.id.nav_institucional
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/example/ecosystem_mobile/views/InstitucionalFragment.kt
+++ app/src/main/java/com/example/ecosystem_mobile/views/InstitucionalFragment.kt
@@ -1,4 +1,34 @@
 package com.example.ecosystem_mobile.views
 
-class InstitucionalFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ecosystem_mobile.databinding.FragmentEditarPerfilBinding
+import com.example.ecosystem_mobile.databinding.FragmentInstitucionalBinding
+
+class InstitucionalFragment : Fragment() {
+
+private var _binding: FragmentInstitucionalBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentInstitucionalBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
