b532640735994fece1182edad30bb72b2353e097
Carlos Augusto Da Silva Santos <code.carlosaugusto@gmail.com>
2024-02-06 21:49:41 -0300

    Continuação das configurações de layout - Tela Institucional

--- app/src/main/java/com/example/ecosystem_mobile/LoginActivity.kt
+++ app/src/main/java/com/example/ecosystem_mobile/LoginActivity.kt
@@ -2,8 +2,13 @@ package com.example.ecosystem_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecosystem_mobile.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
